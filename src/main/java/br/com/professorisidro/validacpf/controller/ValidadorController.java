881aaa19ad207acfa6f7117ad277095c7c3ec6db
vhroeder <vhroeder@gmail.com>
2020-10-08 19:16:48 -0300

    milagre do junin

---
+++ src/main/java/br/com/professorisidro/validacpf/controller/ValidadorController.java
@@ -0,0 +1,27 @@
+package br.com.professorisidro.validacpf.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.professorisidro.validacpf.service.Validador;
+
+@RestController
+@CrossOrigin("*")
+public class ValidadorController {
+	
+	@GetMapping("/validacpf")
+	public String validaCPF(@RequestParam String cpf) {
+		if (Validador.isCPF(cpf)) {
+			return "{\"status\": \"valido\"}";
+		}
+		 return "{\"status\": \"invalido\"}";
+	}
+	@GetMapping("/")
+	public String index() {
+
+		 return "{\"mensagem\": \"bem vindo à API do Victor\"}";
+	}
+
+}
